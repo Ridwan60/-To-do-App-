dc6e63dae8138c1eaef7c1b292be86bd0594d895
Ridwanur Rashid Siam <siam.forcode01@gmail.com>
2023-07-23 04:45:36 +0600

    called api and did authentication with jwt and sql database

--- login/login.js
+++ login/login.js
@@ -3,10 +3,38 @@ const passwordData = document.getElementById('password');
 const loginForm = document.getElementById('loginForm');
 
 loginForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-    // username.value
-    // password.value
-    if(true){
-        window.location.href = "../home/home.html"
-    }
+    event.preventDefault();    
+    login(username.value, password.value);
 });
+
+function login(username, pass) 
+{
+    const idPass = {
+      username: username,
+      password: pass
+    };  
+    
+    fetch('http://localhost:1234/login', {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      }, 
+      body: JSON.stringify(idPass)
+    }).then(response => response.json())
+      .then(data => {      
+        token = data.token;
+        if(token) {
+          console.log('token that you got : ',token);
+          localStorage.setItem('token', token);
+          localStorage.setItem('username', username);
+          window.location.href = "../home/home.html";
+        }
+        else {
+          alert('Invalid credentials')
+        }      
+      })
+      .catch(error => {
+        console.error('Error : ', error);
+        alert("Server error")
+      });
+}
--- signup/signup.js
+++ signup/signup.js
@@ -4,9 +4,34 @@ const signupForm = document.getElementById('signupForm');
 
 signupForm.addEventListener("submit", function(event) {
     event.preventDefault();
-    // username.value
-    // password.value
-    if(true){
-        window.location.href = "../login/login.html"
-    }
+    signup(username.value, password.value);
 });
+
+function signup(username, pass) 
+{
+    const idPass = {
+      username: username,
+      password: pass
+    };  
+    
+    fetch('http://localhost:1234/signup', {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      }, 
+      body: JSON.stringify(idPass)
+    }).then(response => response.json())
+      .then(data => {      
+        message = data.message;
+        if(message === 'Signed up successfully') {
+          window.location.href = "../login/login.html";
+        }
+        else {
+          alert(message)
+        }      
+      })
+      .catch(error => {
+        console.error('Error : ', error);
+        alert("Server error")
+      });
+}
