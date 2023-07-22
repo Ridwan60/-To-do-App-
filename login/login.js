05822133e7ec4ab3a5920ddefe3e380c14b0703a
Ridwanur Rashid Siam <siam.forcode01@gmail.com>
2023-07-23 02:09:26 +0600

    added controllers in login js

---
+++ login/login.js
@@ -0,0 +1,12 @@
+const userNameData = document.getElementById('username');
+const passwordData = document.getElementById('password');
+const loginForm = document.getElementById('loginForm');
+
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    // username.value
+    // password.value
+    if(true){
+        window.location.href = "../home/home.html"
+    }
+});
